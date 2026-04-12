9b6c3acc0ba02767c9b1bb8725a7278104ee1f80
Sakshi <skmodi2005gmail.com>
2026-04-13 02:38:41 +0530

    register admin models and whitenoise

--- happypaws/settings.py
+++ happypaws/settings.py
@@ -75,6 +75,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # ← add this line
+
 ]
 
 ROOT_URLCONF = 'happypaws.urls'
--- myadmin/admin.py
+++ myadmin/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User, Pet, Shelter, Product_form, Order_table, Feedback, PetHealth, ChatHistory, PasswordResetToken
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Pet)
+admin.site.register(Shelter)
+admin.site.register(Product_form)
+admin.site.register(Order_table)
+admin.site.register(Feedback)
+admin.site.register(PetHealth)
+admin.site.register(ChatHistory)
+admin.site.register(PasswordResetToken)
