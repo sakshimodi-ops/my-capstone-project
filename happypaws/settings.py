d263e7da5d9446598fa7c2b75e4ecc9fe492bf2b
Sakshi <skmodi2005gmail.com>
2026-04-13 07:57:09 +0530

    fix database url config

--- happypaws/settings.py
+++ happypaws/settings.py
@@ -99,29 +99,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'happypaws.wsgi.application'
 
-DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': "happypaws",
-            "USER": "root",
-            "PASSWORD": "",
-            "HOST": "127.0.0.1",
-            "PORT": "3306",
-        }
-    }
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-# import dj_database_url
-
-# if os.getenv("DATABASE_URL"):
-#     # ✅ Production (Render)
-#     DATABASES = {
-#         'default': dj_database_url.parse(os.getenv("DATABASE_URL"))
-#     }
-# else:
-#     # ✅ Local (your MySQL)
-#     DATABASES = {
+# DATABASES = {
 #         'default': {
 #             'ENGINE': 'django.db.backends.mysql',
 #             'NAME': "happypaws",
@@ -131,6 +109,30 @@ DATABASES = {
 #             "PORT": "3306",
 #         }
 #     }
+
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+import dj_database_url
+
+if os.getenv("DATABASE_URL"):
+    # ✅ Production (Render)
+    DATABASES = {
+        'default': dj_database_url.parse(os.getenv("DATABASE_URL"))
+    }
+else:
+    # ✅ Local (your MySQL)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': "happypaws",
+            "USER": "root",
+            "PASSWORD": "",
+            "HOST": "127.0.0.1",
+            "PORT": "3306",
+        }
+    }
 # import os
 
 # DATABASES = {
